ec9baa568ad632d5be0d737b49e446214e36198b
Dario <krugerdario1@gmail.com>
2022-03-07 21:51:30 -0300

    Comentarios

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
 /* jQuery */
+//$(): Usado em todas as funções que devem ser referenciadas no jQuery.
 $(document).ready(function(){
+    /// ready(): Associado a leitura do documento enquanto ele é carregado.
     $("input").blur(function(){
+        /// blur(): Associado quando o input perde o foco.
      if($(this).val() == ""){
+         /// usado para retornar o valor ou definir um valor do elemento
              $(this).css({"border" : "1px solid #F00"});
          }else{
             $(this).css({"border" : "1px solid #0000"});
+            /// método define ou retorna uma ou mais propriedades de estilo para os elementos selecionados.
          }
     });
     $("#button").click(function(){
+        ///aciona o evento click ou anexa uma função para ser executada quando ocorre um evento click
      var cont = 0;
      $("#formulario").each(function(){
         if($(this).val() == ""){
